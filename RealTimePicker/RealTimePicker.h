5353920eece6c69bf302d8b1f34cca4e754e6826
Admin <zholayev.t@gmail.com>
2022-04-24 15:00:41 +0600

    Initial project setup

---
+++ RealTimePicker/RealTimePicker.h
@@ -0,0 +1,18 @@
+//
+//  RealTimePicker.h
+//  RealTimePicker
+//
+//  Created by Admin on 24.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RealTimePicker.
+FOUNDATION_EXPORT double RealTimePickerVersionNumber;
+
+//! Project version string for RealTimePicker.
+FOUNDATION_EXPORT const unsigned char RealTimePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealTimePicker/PublicHeader.h>
+
+
---
+++ RealTimePickerExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTimePickerExamples
+//
+//  Created by Admin on 24.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
