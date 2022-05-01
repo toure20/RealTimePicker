564cb83f7ce9a897c7c642a6355a2f12abce12e2
Admin <zholayev.t@gmail.com>
2022-05-02 02:57:00 +0600

    feat: example screens added

--- RealTimePickerExamples/ExampleViewController.swift
+++ RealTimePickerExamples/ExampleViewController.swift
@@ -10,25 +10,44 @@ import RealTimePicker
 
 class ExampleViewController: UIViewController {
     
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "RealTimePickerView"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .black
+        return label
+    }()
+    
+    private var bgImageView: UIImageView = {
+        let imgView = UIImageView(image: UIImage(named: "main_bg"))
+        imgView.contentMode = .scaleAspectFill
+        imgView.clipsToBounds = true
+        return imgView
+    }()
+    
     private var selectedLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 24)
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
         label.text = "Select time"
+        label.textColor = .white
         return label
     }()
     
     private var timePicker: RealTimePickerView = {
         let view = RealTimePickerView(format: .h24)
         view.rowHeight = 60 // default row height is 60
-        view.timeLabelFont = UIFont.systemFont(ofSize: 44, weight: .semibold)
-        view.colonLabelFont = UIFont.systemFont(ofSize: 44 * 0.75, weight: .bold)
-        view.formatLabelFont = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        view.timeLabelFont = UIFont.systemFont(ofSize: 44, weight: .semibold) // default size
+        view.colonLabelFont = UIFont.systemFont(ofSize: 44 * 0.75, weight: .bold) // default size
+        view.formatLabelFont = UIFont.systemFont(ofSize: 24, weight: .semibold) // default size
+        view.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+        view.layer.cornerRadius = 24
         return view
     }()
     
     private lazy var segmentControl: UISegmentedControl = {
         let view = UISegmentedControl(items: ["24-hour", "12-hour"])
         view.addTarget(self, action: #selector(didSelectSegment(_:)), for: .valueChanged)
+        view.backgroundColor = .white
         view.selectedSegmentIndex = 0
         return view
     }()
@@ -42,6 +61,18 @@ class ExampleViewController: UIViewController {
     
     private func setupViews() {
         view.backgroundColor = UIColor.white
+        
+        view.addSubview(bgImageView)
+        bgImageView.translatesAutoresizingMaskIntoConstraints = false
+        bgImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        bgImageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        bgImageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        bgImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
         
         view.addSubview(timePicker)
         timePicker.translatesAutoresizingMaskIntoConstraints = false
